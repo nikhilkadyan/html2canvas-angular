740d35e8806e8673e88e9a54c35caef0654d4f24
nikhilkadyan <nikhilkadyan@outlook.in>
2020-03-04 14:36:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# html2canvas-angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/html2canvas-angular)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+declare let html2canvas: any;
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+  save(fileName){
+    // First we get our section to save from dom
+    let section = document.querySelector('#mainContainer');
+
+    // We pass that section to html2Canvase
+    html2canvas(section).then(canvas => {
+      var link = document.createElement('a');
+      link.href = canvas.toDataURL();
+      link.download = fileName;
+      document.body.appendChild(link);
+      link.click();
+    });
+  }
+  
+}
